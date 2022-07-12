05892d4c546f36534a4ad608dda6667e36693aeb
desmundyeng <desmundyeng@gmail.com>
2022-07-12 14:52:45 +0800

    release 1.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ publishing {
         create<MavenPublication>("auraTranslator") {
             groupId = "net.georgekent.lib"
             artifactId = "aura_translator"
-            version = "1.3.0"
+            version = "1.4.0"
             from(components["java"])
         }
     }
