1c9bf5e4b37b17610013017ee8a58ebaeb3a3e19
desmundyeng <desmundyeng@gmail.com>
2022-09-29 14:37:56 +0800

    update Alarm translator to include nonRealtimeBackFlowDetection, rebootDetection, backFlowReading

--- src/main/kotlin/AlarmTranslator.kt
+++ src/main/kotlin/AlarmTranslator.kt
@@ -2,15 +2,21 @@ import Util.decodeHex
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.cbor.databind.CBORMapper
 import model.Alarm
+import model.AlarmParameter
+import model.AlarmRaw
 
 /* This is a translator for LWM2M object 10377 instance 0 Data object */
 object AlarmTranslator {
 
-    private fun translate(input: String): Int {
+    private fun translate(input: String): AlarmRaw {
         val mapper: ObjectMapper = CBORMapper()
         val byteArrayValueShort = input.decodeHex()
         val offset = 0
-        return mapper.readValue(byteArrayValueShort, offset, byteArrayValueShort.size, Int::class.java)
+        val list = mapper.readValue(byteArrayValueShort, offset, byteArrayValueShort.size, List::class.java)
+        val alarmRaw = list[0] as Int
+        val alarmBinary = alarmRaw.toString(2).toLong()
+        val backFlowReading = list[1] as Float
+        return AlarmRaw(alarmBinary, backFlowReading)
     }
 
     fun toObject(input: String): Alarm {
@@ -18,14 +24,14 @@ object AlarmTranslator {
         return Alarm(rawAlarm)
     }
 
-    fun toNodes(input: String): ArrayList<Int> {
-        val alarms = ArrayList<Int>()
+    fun toNodes(input: String): ArrayList<Number> {
+        val alarms = ArrayList<Number>()
         val rawAlarm = translate(input)
-        (rawAlarm + 1000000000).toString().subSequence(1..9).toString()
-            //.reversed()
+        (rawAlarm.alarm + AlarmParameter.OFFSET).toString().subSequence(1..AlarmParameter.NUMBER_OF_ALARMS)
             .map {
                 alarms.add(it.digitToInt())
             }
+        alarms.add(rawAlarm.backFlowReading)
         return alarms
     }
 
--- src/main/kotlin/model/Alarm.kt
+++ src/main/kotlin/model/Alarm.kt
@@ -1,6 +1,18 @@
 package model
 
+import model.AlarmParameter.NUMBER_OF_ALARMS
+import kotlin.math.pow
+
+object AlarmParameter {
+    const val NUMBER_OF_ALARMS = 11
+    val OFFSET = 10.0.pow(NUMBER_OF_ALARMS).toLong()
+}
+
+data class AlarmRaw(val alarm: Long, val backFlowReading: Float)
+
 data class Alarm(
+    var nonRealtimeBackFlowDetection: Boolean = false,
+    var rebootDetection: Boolean = false,
     var leakageDetection: Boolean = false,
     var noFlowDetection: Boolean = false,
     var burstDetection: Boolean = false,
@@ -9,22 +21,25 @@ data class Alarm(
     var faultySensor: Boolean = false,
     var wireCutDetection: Boolean = false,
     var tiltDetection: Boolean = false,
-    var magnetTamper: Boolean = false
+    var magnetTamper: Boolean = false,
+    var backFlowReading: Float = 0.0f
 ) {
-    constructor(alarm: Int) : this() {
-        val rawAlarm = (alarm + 1000000000).toString().subSequence(1..9)
-            //.reversed()
+    constructor(alarm: AlarmRaw) : this() {
+        val rawAlarm = (alarm.alarm + AlarmParameter.OFFSET).toString().subSequence(1..NUMBER_OF_ALARMS)
             .map {
-            it.digitToInt() == 1
-        }
-        magnetTamper = rawAlarm[8]
-        tiltDetection = rawAlarm[7]
-        wireCutDetection = rawAlarm[6]
-        faultySensor = rawAlarm[5]
-        batteryLow = rawAlarm[4]
-        backFlowDetection = rawAlarm[3]
-        burstDetection = rawAlarm[2]
-        noFlowDetection = rawAlarm[1]
-        leakageDetection = rawAlarm[0]
+                it.digitToInt() == 1
+            }
+        magnetTamper = rawAlarm[10]
+        tiltDetection = rawAlarm[9]
+        wireCutDetection = rawAlarm[8]
+        faultySensor = rawAlarm[7]
+        batteryLow = rawAlarm[6]
+        backFlowDetection = rawAlarm[5]
+        burstDetection = rawAlarm[4]
+        noFlowDetection = rawAlarm[3]
+        leakageDetection = rawAlarm[2]
+        rebootDetection = rawAlarm[1]
+        nonRealtimeBackFlowDetection = rawAlarm[0]
+        backFlowReading = alarm.backFlowReading
     }
 }
