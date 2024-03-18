00bcf64192f11615ef2de1aa81cb199946058457
desmundyeng <19725937+desmundyeng@users.noreply.github.com>
2024-03-18 16:56:42 +0800

    Update jvm and library version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,20 @@
 plugins {
-    kotlin("jvm") version "1.6.20"
-    kotlin("plugin.serialization") version "1.6.20"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.serialization") version "1.9.22"
 
     `java-library`
     `maven-publish`
 }
 
 group = "net.georgekent.lib.aura_translator"
-version = "1.5.0"
+version = "1.5.1"
 
 publishing {
     publications {
         create<MavenPublication>("auraTranslator") {
             groupId = "net.georgekent.lib"
             artifactId = "aura_translator"
-            version = "1.5.0"
+            version = "1.5.1"
             from(components["java"])
         }
     }
@@ -35,7 +35,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.3.2")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-cbor:2.13.2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-cbor:2.17.0")
     implementation("junit:junit:4.13.1")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.7.0")
